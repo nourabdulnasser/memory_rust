5e1e79ee711f9110d8afb67aa4355e3091f0c9b7
nourabdulnasser <85139658+nourabdulnasser@users.noreply.github.com>
2022-04-25 23:36:58 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod cards;
+
+use std::{thread, time};
+use std::env;
+use std::process::Command;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<_> = env::args().collect();
+
+    if args[1] == "cards" {
+        let  num_cards : usize = args[2].parse().unwrap();
+        let  sec_per_card : usize = args[3].parse().unwrap();
+        cards::cards_memory(num_cards, sec_per_card);
+        //println!("{}", num_cards);
+    } else {
+        println!("a different game");
+    }; 
+    
+    
+    
+    
 }
